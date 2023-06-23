c20015f09040792e91392172f41f86c3bb8e1fef
Andrei T <andrey7titov@gmail.com>
2023-06-23 16:35:35 +0300

    Add 01-basics/02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+createApp({
+    data() {
+        return {
+            firstNumber: 0,
+            secondNumber: 0,
+            operator: ''
+        }
+    },
+
+    computed: {
+        result() {
+            switch(this.operator) {
+                case "sum":
+                    return this.firstNumber + this.secondNumber
+                case "subtract":
+                    return this.firstNumber - this.secondNumber
+                case "multiply":
+                    return this.firstNumber * this.secondNumber
+                case "divide":
+                    return this.firstNumber / this.secondNumber
+            }            
+        }
+    }
+
+
+}).mount('#app')
